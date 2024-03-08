98ef94092811ca78718c8f119219f01a4c055fef
Mikolaj <bubus258@gmail.com>
2024-03-09 00:36:38 +0100

    fix utility pruning functions

--- pruning/architecture/utility/pruning.py
+++ pruning/architecture/utility/pruning.py
@@ -3,18 +3,39 @@ import torch.nn as nn
 from typing import Iterable
 
 
-def get_parameters_to_prune(model: nn.Module) -> list[nn.Parameter]:
+def get_parameters_to_prune(
+    model: nn.Module, types_to_prune: tuple[nn.Module]
+) -> list[tuple[nn.Module, str]]:
+    """Get the parameters to prune from a model.
+
+    Args:
+        model (nn.Module): A PyTorch model.
+        types_to_prune (tuple[nn.Module]): Tuple of module types to prune. Ex. nn.Linear, nn.Conv2d.
+
+    Returns:
+        list[tuple[nn.Module, str]]: List of tuples containing the module and the parameter name.
+    """
     return [
-        (module, "weight")
+        (module, name)
         for module in model.modules()
-        if any(param.requires_grad for param in module.parameters())
-        and len(list(module.children())) == 0
+        if isinstance(module, types_to_prune)
+        for name, param in module.named_parameters()
+        if param.requires_grad
     ]
 
 
-def calculate_total_sparsity(
+def calculate_parameters_sparsity(
     model: nn.Module, parameters_to_prune: Iterable[tuple[nn.Module, str]]
 ) -> float:
+    """Calculate the total sparsity of the model for given parameters.
+
+    Args:
+        model (nn.Module): A PyTorch model to calculate the sparsity of.
+        parameters_to_prune (Iterable[tuple[nn.Module, str]]): Iterable of parameters which are pruned.
+
+    Returns:
+        float: Percentage of zero weights in range from 0 to 100%.
+    """
     total_weights = 0
     total_zero_weights = 0
 
@@ -25,11 +46,32 @@ def calculate_total_sparsity(
             if (module, param_name) not in pruned_parameters:
                 continue
 
-            if "weight" in param_name:
-                total_weights += float(param.nelement())
-                total_zero_weights += float(torch.sum(param == 0))
+            total_weights += float(param.nelement())
+            total_zero_weights += float(torch.sum(param == 0))
 
-    return total_zero_weights / total_weights
+    return (total_zero_weights / total_weights) * 100
+
+
+def calculate_total_sparsity(model: nn.Module) -> float:
+    """Calculate the total sparsity of the model.
+    Args:
+        model (nn.Module): A PyTorch model to calculate the sparsity of.
+
+    Returns:
+        float: Percentage of zero weights in range from 0 to 100%.
+    """
+    total_weights = 0
+    total_zero_weights = 0
+
+    for module in model.modules():
+        for param in module.parameters():
+            if not param.requires_grad:
+                continue
+
+            total_weights += float(param.nelement())
+            total_zero_weights += float(torch.sum(param == 0))
+
+    return (total_zero_weights / total_weights) * 100
 
 
 def calculate_parameters_amount(modules: Iterable[tuple[nn.Module, str]]) -> int:
