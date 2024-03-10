47a67fa364abaa1b9060c6214533fd2a4f8b4f1b
Mikolaj <bubus258@gmail.com>
2024-03-10 18:41:01 +0100

    add number of classes parameter to config

--- pruning/architecture/construct_model.py
+++ pruning/architecture/construct_model.py
@@ -5,20 +5,20 @@ from omegaconf import DictConfig
 from torch import nn
 
 
-def construct_lenet(path: str) -> nn.Module:
+def construct_lenet(path: str, num_classes: int) -> nn.Module:
     model = LeNet()
     model.load_state_dict(torch.load(path))
     return model
 
 
-def construct_resnet50(path: str) -> nn.Module:
-    model = ResNet50()
+def construct_resnet50(path: str, num_classes: int) -> nn.Module:
+    model = ResNet50(num_classes)
     model.load_state_dict(torch.load(path))
     return model
 
 
-def construct_resnet18(path: str) -> nn.Module:
-    model = ResNet18()
+def construct_resnet18(path: str, num_classes: int) -> nn.Module:
+    model = ResNet18(num_classes)
     model.load_state_dict(torch.load(path))
     return model
 
@@ -26,11 +26,11 @@ def construct_resnet18(path: str) -> nn.Module:
 def construct_model(cfg: DictConfig) -> nn.Module:
     match (cfg.model.name.lower(), cfg.dataset.name.lower()):
         case ("lenet", _):
-            return construct_lenet(cfg.model.path)
+            return construct_lenet(cfg.model.path, cfg.dataset.num_classes)
         case ("resnet50", _):
-            return construct_resnet50(cfg.model.path)
+            return construct_resnet50(cfg.model.path, cfg.dataset.num_classes)
         case ("resnet18", _):
-            return construct_resnet18(cfg.model.path)
+            return construct_resnet18(cfg.model.path, cfg.dataset.num_classes)
         case _:
             raise ValueError(
                 f"Unknown model: {cfg.model.name} for dataset: {cfg.dataset.name}"
