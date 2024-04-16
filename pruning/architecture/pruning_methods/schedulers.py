314fc034b3293a58605dca817db4be0ddcb3d712
Kowalski1024 <tomasz_wojnar@hotmail.com>
2024-04-16 19:27:59 +0200

    IterativeStepScheduler fix

--- pruning/architecture/pruning_methods/schedulers.py
+++ pruning/architecture/pruning_methods/schedulers.py
@@ -45,7 +45,7 @@ class IterativeStepScheduler(BasePruningStepScheduler):
         while pruned_count < self.end * self.inital_param_count:
             current_step = int(self.step * (self.inital_param_count - pruned_count))
 
-            assert current_step <= 0, "The pruning step is too small."
+            assert current_step > 0, "The pruning step is too small."
 
             pruned_count += current_step
             yield current_step
