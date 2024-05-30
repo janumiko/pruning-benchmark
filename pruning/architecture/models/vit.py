30ce46b68905da4df16234f9dbd4af097ea520b5
Kowalski1024 <tomasz_wojnar@hotmail.com>
2024-05-30 15:37:30 +0200

    remove file

--- pruning/architecture/models/vit.py
+++
@@ -1,9 +0,0 @@
-from timm import create_model
-from timm.models.registry import register_model
-
-
-@register_model
-def vit_small_patch16_224(num_classes: int, **kwargs):
-    model = create_model("vit_small_patch16_224", num_classes=num_classes)
-
-    return model
